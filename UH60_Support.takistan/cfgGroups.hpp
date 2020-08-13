b985875eca3f656394fa676a9686b1c132078569
Donovan Cunningham <donovancunningham@rocketmail.com>
2020-08-13 17:51:25 -0400

    Added UH60M_Support
    A Support Mission for Aviation to practice with

---
+++ UH60_Support.takistan/cfgGroups.hpp
@@ -0,0 +1,217 @@
+class CfgGroups
+{
+	class West
+	{
+		name = $STR_A3_CfgGroups_West0;
+		side = TWest;
+		class BLU_F
+		{
+			name = $STR_A3_CfgGroups_West_BLU_F0;
+			class Infantry
+			{
+				name = $STR_A3_CfgGroups_West_BLU_F_Infantry0;
+				class BUS_InfSquad8
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Infantry_BUS_InfSquad0;
+					side = TWest;
+					faction = BLU_F;
+					class Unit0	{side = TWest; vehicle = rhsusf_army_ocp_squadleader; rank = "SERGEANT";};
+					class Unit1	{side = TWest; vehicle = rhsusf_army_ocp_teamleader; rank = "SERGEANT";};
+					class Unit2	{side = TWest; vehicle = rhsusf_army_ocp_rifleman; rank = "CORPORAL";};
+					class Unit3	{side = TWest; vehicle = rhsusf_army_ocp_autorifleman; rank = "PRIVATE";};
+					class Unit4	{side = TWest; vehicle = rhsusf_army_ocp_grenadier; rank = "PRIVATE";};
+					class Unit5	{side = TWest; vehicle = rhsusf_army_ocp_squadleader; rank = "SERGEANT";};
+					class Unit6	{side = TWest; vehicle = rhsusf_army_ocp_teamleader; rank = "SERGEANT";};
+					class Unit7	{side = TWest; vehicle = rhsusf_army_ocp_rifleman; rank = "CORPORAL";};
+					class Unit8	{side = TWest; vehicle = rhsusf_army_ocp_autorifleman; rank = "PRIVATE";};
+					class Unit9	{side = TWest; vehicle = rhsusf_army_ocp_riflemanat; rank = "PRIVATE";};
+				};
+				class BUS_InfSquad16
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Infantry_BUS_InfSquad0;
+					side = TWest;
+					faction = BLU_F;
+					class Unit0	{side = TWest; vehicle = rhsusf_army_ocp_squadleader; rank = "SERGEANT";};
+					class Unit1	{side = TWest; vehicle = rhsusf_army_ocp_teamleader; rank = "SERGEANT";};
+					class Unit2	{side = TWest; vehicle = rhsusf_army_ocp_rifleman; rank = "CORPORAL";};
+					class Unit3	{side = TWest; vehicle = rhsusf_army_ocp_autorifleman; rank = "PRIVATE";};
+					class Unit4	{side = TWest; vehicle = rhsusf_army_ocp_grenadier; rank = "PRIVATE";};
+					class Unit5	{side = TWest; vehicle = rhsusf_army_ocp_squadleader; rank = "SERGEANT";};
+					class Unit6	{side = TWest; vehicle = rhsusf_army_ocp_teamleader; rank = "SERGEANT";};
+					class Unit7	{side = TWest; vehicle = rhsusf_army_ocp_rifleman; rank = "CORPORAL";};
+					class Unit8	{side = TWest; vehicle = rhsusf_army_ocp_autorifleman; rank = "PRIVATE";};
+					class Unit9	{side = TWest; vehicle = rhsusf_army_ocp_riflemanat; rank = "PRIVATE";};
+				};
+				class BUS_InfTeam_AT
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Infantry_BUS_InfTeam_AT0;
+					side = TWest;
+					faction = BLU_F;
+					class Unit0	{side = TWest; vehicle = rhsusf_army_ocp_squadleader; rank = "SERGEANT";};
+					class Unit1	{side = TWest; vehicle = rhsusf_army_ocp_teamleader; rank = "SERGEANT";};
+					class Unit2	{side = TWest; vehicle = rhsusf_army_ocp_rifleman; rank = "CORPORAL";};
+					class Unit3	{side = TWest; vehicle = rhsusf_army_ocp_autorifleman; rank = "PRIVATE";};
+					class Unit4	{side = TWest; vehicle = rhsusf_army_ocp_riflemanat; rank = "PRIVATE";};
+				};
+			};
+			class Motorized
+			{
+				name = $STR_A3_CfgGroups_West_BLU_F_Motorized0;
+				class BUS_MotInf_AT
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Motorized_BUS_MotInf_AT0;
+					side = TWest;
+					faction = BLU_F;
+					class Unit0	{side = TWest; vehicle = rhsusf_m1240a1_mk19_usarmy_d; rank = "SERGEANT";};
+					class Unit1	{side = TWest; vehicle = rhsusf_army_ocp_riflemanat; rank = "CORPORAL";};
+					class Unit2	{side = TWest; vehicle = rhsusf_army_ocp_riflemanat; rank = "CORPORAL";};
+					class Unit3	{side = TWest; vehicle = rhsusf_army_ocp_autorifleman; rank = "PRIVATE";};
+				};
+				class BUS_MotInf_GMG
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Motorized_BUS_MotInf_Team0;
+					side = TWest;
+					faction = BLU_F;
+					class Unit0	{side = TWest; vehicle = rhsusf_m1240a1_m2_usarmy_d; rank = "SERGEANT";};
+					class Unit1	{side = TWest; vehicle = rhsusf_army_ocp_riflemanat; rank = "CORPORAL";};
+					class Unit2	{side = TWest; vehicle = rhsusf_army_ocp_riflemanat; rank = "CORPORAL";};
+					class Unit3	{side = TWest; vehicle = rhsusf_army_ocp_autorifleman; rank = "PRIVATE";};
+				};
+				class BUS_MotInf_HMG
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Motorized_BUS_MotInf_Team0;
+					side = TWest;
+					faction = BLU_F;
+					class Unit0	{side = TWest; vehicle = rhsusf_m1240a1_m2_usarmy_d; rank = "SERGEANT";};
+					class Unit1	{side = TWest; vehicle = rhsusf_army_ocp_riflemanat; rank = "CORPORAL";};
+					class Unit2	{side = TWest; vehicle = rhsusf_army_ocp_riflemanat; rank = "CORPORAL";};
+					class Unit3	{side = TWest; vehicle = rhsusf_army_ocp_autorifleman; rank = "PRIVATE";};
+				};
+			};
+			class Mechanized
+			{
+				name = $STR_A3_CfgGroups_West_BLU_F_Mechanized0;
+				class BUS_MechInfSquad
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Mechanized_BUS_MechInfSquad0;
+					side = TWest;
+					faction = BLU_F;
+					class Unit0	{side = TWest; vehicle = rhsusf_stryker_m1126_m2_d; rank = "SERGEANT";};
+					class Unit1	{side = TWest; vehicle = rhsusf_army_ocp_squadleader; rank = "SERGEANT";};
+					class Unit2	{side = TWest; vehicle = rhsusf_army_ocp_teamleader; rank = "SERGEANT";};
+					class Unit3	{side = TWest; vehicle = rhsusf_army_ocp_rifleman; rank = "CORPORAL";};
+					class Unit4	{side = TWest; vehicle = rhsusf_army_ocp_autorifleman; rank = "PRIVATE";};
+					class Unit5	{side = TWest; vehicle = rhsusf_army_ocp_grenadier; rank = "PRIVATE";};
+				};
+			};
+		};
+	};
+	class East
+	{
+		name = $STR_A3_CfgGroups_East0;
+		side = TEast;
+		class OPF_F
+		{
+			name = $STR_A3_CfgGroups_East_OPF_F0;
+			class Infantry
+			{
+				name = $STR_A3_CfgGroups_West_BLU_F_Infantry0;
+				class OIA_InfSquad8
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Infantry_BUS_InfSquad0;
+					side = TEast;
+					faction = OPF_F;
+					class Unit0	{side = TEast; vehicle = rhs_msv_sergeant; rank = "SERGEANT";};
+					class Unit1	{side = TEast; vehicle = rhs_msv_efreitor; rank = "SERGEANT";};
+					class Unit2	{side = TEast; vehicle = rhs_msv_grenadier_rpg; rank = "CORPORAL";};
+					class Unit3	{side = TEast; vehicle = rhs_msv_strelok_rpg_assist; rank = "PRIVATE";};
+					class Unit4	{side = TEast; vehicle = rhs_msv_machinegunner; rank = "PRIVATE";};
+					class Unit5	{side = TEast; vehicle = rhs_msv_LAT; rank = "PRIVATE";};
+					class Unit6	{side = TEast; vehicle = rhs_msv_strelok_rpg_assist; rank = "PRIVATE";};
+					class Unit7	{side = TEast; vehicle = rhs_msv_rifleman; rank = "PRIVATE";};
+					class Unit8	{side = TEast; vehicle = rhs_msv_rifleman; rank = "PRIVATE";};
+					class Unit9	{side = TEast; vehicle = rhs_msv_medic; rank = "PRIVATE";};
+				};	
+				class OIA_InfSquad16
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Infantry_BUS_InfSquad_Weapons0;
+					side = TEast;
+					faction = OPF_F;
+					class Unit0	{side = TEast; vehicle = rhs_msv_sergeant; rank = "SERGEANT";};
+					class Unit1	{side = TEast; vehicle = rhs_msv_efreitor; rank = "SERGEANT";};
+					class Unit2	{side = TEast; vehicle = rhs_msv_grenadier_rpg; rank = "CORPORAL";};
+					class Unit3	{side = TEast; vehicle = rhs_msv_strelok_rpg_assist; rank = "PRIVATE";};
+					class Unit4	{side = TEast; vehicle = rhs_msv_machinegunner; rank = "PRIVATE";};
+					class Unit5	{side = TEast; vehicle = rhs_msv_LAT; rank = "PRIVATE";};
+					class Unit6	{side = TEast; vehicle = rhs_msv_strelok_rpg_assist; rank = "PRIVATE";};
+					class Unit7	{side = TEast; vehicle = rhs_msv_rifleman; rank = "PRIVATE";};
+					class Unit8	{side = TEast; vehicle = rhs_msv_rifleman; rank = "PRIVATE";};
+					class Unit9	{side = TEast; vehicle = rhs_msv_medic; rank = "PRIVATE";};
+				};
+				class OIA_InfTeam_AT
+				{
+					name = $STR_A3_CfgGroups_West_BLU_F_Infantry_BUS_InfTeam_AT0;
+					side = TEast;
+					faction = OPF_F;
+					class Unit0	{side = TEast; vehicle = rhs_msv_efreitor; rank = "SERGEANT";};
+					class Unit1	{side = TEast; vehicle = rhs_msv_grenadier_rpg; rank = "CORPORAL";};
+					class Unit2	{side = TEast; vehicle = rhs_msv_machinegunner; rank = "PRIVATE";};
+					class Unit3	{side = TEast; vehicle = rhs_msv_LAT; rank = "PRIVATE";};
+					
+				};
+			};
+			class Motorized
+			{
+				name = $STR_A3_cfggroups_East_OPF_F_Motorized_MTP0;
+				class OIA_MotInf_Team_GMG
+				{
+					name = $STR_A3_cfggroups_East_OPF_F_Motorized_MTP_OIA_MotInfTeam0;
+					side = TEast;
+					faction = OPF_F;
+					class Unit0	{side = TEast; vehicle = rhs_tigr_sts_3camo_vmf; rank = "SERGEANT";};
+					class Unit1	{side = TEast; vehicle = rhs_msv_rifleman; rank = "PRIVATE";};
+					class Unit2	{side = TEast; vehicle = rhs_msv_rifleman; rank = "PRIVATE";};
+				};
+				class OIA_MotInf_Team_HMG
+				{
+					name = $STR_A3_cfggroups_East_OPF_F_Motorized_MTP_OIA_MotInfTeam0;
+					side = TEast;
+					faction = OPF_F;
+					class Unit0	{side = TEast; vehicle = rhs_tigr_sts_3camo_vmf; rank = "SERGEANT";};
+					class Unit1	{side = TEast; vehicle = rhs_msv_rifleman; rank = "PRIVATE";};
+					class Unit2	{side = TEast; vehicle = rhs_msv_rifleman; rank = "PRIVATE";};
+				};
+				class OIA_MotInf_AT
+				{
+					name = $STR_A3_cfggroups_East_OPF_F_Motorized_MTP_OIA_MotInf_AT0;
+					side = TEast;
+					faction = OPF_F;
+					class Unit0	{side = TEast; vehicle = rhs_tigr_sts_3camo_vmf; rank = "SERGEANT";};
+					class Unit1	{side = TEast; vehicle = rhs_msv_rifleman; rank = "PRIVATE";};
+					class Unit2	{side = TEast; vehicle = rhs_msv_rifleman; rank = "PRIVATE";};
+				};
+			};
+			class Mechanized
+			{
+				name = $STR_A3_CfgGroups_West_BLU_F_Mechanized0;
+				class OIA_MechInfSquad
+				{
+					name = $STR_A3_cfggroups_West_BLU_F_Mechanized_BUS_MechInfSquad0;
+					side = TEast;
+					faction = OPF_F;
+					class Unit0	{side = TEast; vehicle = rhs_btr80_vmf; rank = "SERGEANT";};
+					class Unit1	{side = TEast; vehicle = rhs_msv_sergeant; rank = "SERGEANT";};
+					class Unit2	{side = TEast; vehicle = rhs_msv_efreitor; rank = "SERGEANT";};
+					class Unit3	{side = TEast; vehicle = rhs_msv_grenadier_rpg; rank = "CORPORAL";};
+					class Unit4	{side = TEast; vehicle = rhs_msv_strelok_rpg_assist; rank = "PRIVATE";};
+					class Unit5	{side = TEast; vehicle = rhs_msv_machinegunner; rank = "PRIVATE";};
+					class Unit6	{side = TEast; vehicle = rhs_msv_LAT; rank = "PRIVATE";};
+					class Unit7	{side = TEast; vehicle = rhs_msv_strelok_rpg_assist; rank = "PRIVATE";};
+					class Unit8	{side = TEast; vehicle = rhs_msv_rifleman; rank = "PRIVATE";};
+					class Unit9	{side = TEast; vehicle = rhs_msv_rifleman; rank = "PRIVATE";};
+					class Unit10{side = TEast; vehicle = rhs_msv_medic; rank = "PRIVATE";};
+				};
+			};
+		};
+	};
+};
